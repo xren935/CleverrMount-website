bbfe00f01358cc53cf4785d7dd92e67010596e84
xren935 <59517115+xren935@users.noreply.github.com>
2020-12-26 01:28:44 -0800

    updated popup

--- cleverr-mount/src/components/popup.jsx
+++ cleverr-mount/src/components/popup.jsx
@@ -7,7 +7,10 @@ class Popup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true
+            show: true,
+            useremail: '',
+            emailError: '',
+            emailValid: false
         };
         
         this.handleClose = this.handleClose.bind(this);
@@ -22,22 +25,50 @@ class Popup extends React.Component {
         this.setState({ show: true });
     };
     
+    submitForm = (event) => {
+        event.preventDefault();
+        if(this.state.emailValid){
+            alert(this.state.useremail);
+        }else{
+            alert("email is invalid!");
+        }
+    }
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.emailError;
+        let emailValid = this.state.emailValid;
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors = emailValid ? '' : ' is invalid';
+        this.setState({emailError: fieldValidationErrors, emailValid: emailValid});
+    }
+
+    setUserEmail = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        // this.setState({[nam]: val});
+        this.setState({[name]: value}, 
+            () => { this.validateField(name, value)});
+    }
+
     render() {
         return(
-            <div style={{boarder: "20px red"}}>
+            <div>
                 {/* <Button variant="primary" onClick={this.handleShow}>Launch modal</Button> */}
                 <Modal show={this.state.show} onHide={this.handleClose}>
                     <div id="popup">
                     <Modal.Header closeButton>
-                        <Modal.Title>Connect with us!</Modal.Title>
+                        <Modal.Title>Enter your email to get 10% off!</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>please enter your email here</Modal.Body>
-                    <Modal.Body><b>name: </b></Modal.Body>
-                    <Modal.Body><b>email: </b></Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>Close</Button>
-                        <Button variant="primary">Submit</Button>
-                    </Modal.Footer>
+                    <form>
+                    <Modal.Body>
+                        <label>
+                            Your Email: 
+                            <input id="email-inputbox" type="text" name="useremail" onChange={this.setUserEmail}/>
+                        </label>
+                        <br></br>
+                        <input id="submit-btn" type="submit" value="Get 10% Off" onClick={this.submitForm}/>
+                    </Modal.Body>
+                    </form>
                     </div>
                 </Modal>
             </div>
